a88441c01f62e0bcd01df6daae1263b615ad44b5
Dina Filippova <dinafilippova@gmail.com>
2022-12-15 19:21:04 +0700

    Найти количество чисел больше нуля в ряду чисел, введенных с консоли

---
+++ Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите несколько чисел, разделенных пробелом: ");
+string text = Console.ReadLine();
+int[] numbers = Array.ConvertAll(text.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+int CountPositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+
+Console.WriteLine(CountPositiveNumbers(numbers));
